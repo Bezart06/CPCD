12f0de66e2835634b6a6b35c0fca040898987fbd
Artem <bezart06@gmail.com>
2025-05-13 09:31:51 +0300

    0.1.1
    ALERT-FIX: Inconsistent annotation for 'WinMain'.

--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ static void cpcd() {
 #endif
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
     cpcd();
     return 0;
 }
